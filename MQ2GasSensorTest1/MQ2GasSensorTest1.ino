54c906e90e1b5dfa2338bbd148a4ae65c2eda206
Debajyoti Bagchi <debajyoti.bagchi@gmail.com>
2018-12-04 13:46:54 +0530

    Initial commit

---
+++ MQ2GasSensorTest1/MQ2GasSensorTest1.ino
@@ -0,0 +1,24 @@
+const byte gasSensor = A0;    // select the input pin for the GAS SENSOR
+
+//variables used
+float gasSensorValue = 0;  // variable to store the value coming from the GAS SENSOR
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  Serial.println("Initializing !!!");
+  pinMode(gasSensor, INPUT);
+  delay(1000);//3 mins wait for stabilizing coil
+  Serial.println("Ready !!!");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  gasSensorValue = analogRead(gasSensor);
+  Serial.print("gasSensorValue:");
+  Serial.println(gasSensorValue);
+
+  delay(1000);
+
+}
